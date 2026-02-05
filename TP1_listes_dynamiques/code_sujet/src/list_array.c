6d3b5bf4415daf8f0cf2b3322590c0a975872355
AlexandreBlanche-Efrei <alexandre.blanche@efrei.fr>
2026-02-05 17:39:28 +0100

    Ajout de la base de code pour le TP1

---
+++ TP1_listes_dynamiques/code_sujet/src/list_array.c
@@ -0,0 +1,20 @@
+#include "list_array.h"
+
+// Allocates a larger array and moves the data to this new array
+void realloc_list(t_list* list) {
+    // TODO
+}
+
+// Shifts the elements of indices index_start .. size-1 one position to the right
+// It is assumed that capacity >= size + 1
+void shift_right(t_list* list, int index_start) {
+    // TODO
+}
+
+// Shifts the elements of indices index_start .. size-1 one position to the left
+void shift_left(t_list* list, int index_start) {
+    // TODO
+}
+
+
+// TODO
---
+++ TP1_listes_dynamiques/code_sujet/src/list_linked.c
@@ -0,0 +1,8 @@
+#include "list_linked.h"
+
+// Returns the address of the cell at the given index
+t_cell * get_cell(t_list *list, int index) {
+    // TODO
+}
+
+// TODO
